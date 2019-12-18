69fbeb08e7b95016bf39b555973c367ddcfc1550
Michael Vanasse <michael@simplecast.com>
2019-12-17 17:43:01 -0700

    remove hex properties

--- mix.exs
+++ mix.exs
@@ -2,32 +2,30 @@ defmodule Concerto.Plug.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :concerto_plug,
-     description: "Plug integration for Concerto",
-     version: "0.1.3",
-     elixir: "~> 1.2",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     package: package(),
-     deps: deps()]
+    [
+      app: :concerto_plug,
+      description: "Plug integration for Concerto",
+      version: "0.1.3",
+      elixir: "~> 1.2",
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      deps: deps()
+    ]
   end
 
   def application do
-    [applications: [:logger]]
+    [
+      applications: [:logger]
+    ]
   end
 
   defp deps do
-    [{:concerto, ">= 0.0.0"},
-     {:plug, ">= 0.0.0"},
-     {:mazurka, ">= 1.0.0", only: [:dev, :test]},
-     {:mazurka_plug, ">= 0.0.0", only: [:dev, :test]},
-     {:ex_doc, ">= 0.0.0", only: :dev}]
-  end
-
-  defp package do
-    [files: ["lib", "mix.exs", "README*"],
-     maintainers: ["Cameron Bytheway"],
-     licenses: ["MIT"],
-     links: %{"GitHub" => "https://github.com/exstruct/concerto_plug"}]
+    [
+      {:concerto, ">= 0.0.0"},
+      {:plug, ">= 0.0.0"},
+      {:mazurka, ">= 1.0.0", only: [:dev, :test]},
+      {:mazurka_plug, ">= 0.0.0", only: [:dev, :test]},
+      {:ex_doc, ">= 0.0.0", only: :dev}
+    ]
   end
 end
